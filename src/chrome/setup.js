c5ae4e24385df2343a54744ad13590374e834573
Ole Krüger <ole@kru.gr>
2018-11-22 15:38:30 +0100

    Never use sandbox
    Running chrome in a docker environment can not work with sandboxing, so we disable it here

--- src/chrome/setup.js
+++ src/chrome/setup.js
@@ -2,7 +2,7 @@ import launchChrome from '@serverless-chrome/lambda';
 import request from 'superagent';
 
 export async function getChrome() {
-  const chrome = await launchChrome({flags: ['--headless']});
+  const chrome = await launchChrome({flags: ['--no-sandbox', '--headless']});
 
   const response = await request
       .get(`${chrome.url}/json/version`)
